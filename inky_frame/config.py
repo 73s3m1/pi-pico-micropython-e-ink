163ecb639afe0fa205c4e394cb69e2d3d3ef9e5a
73s3m1 <s.missbach@posteo.de>
2024-08-17 22:35:28 +0200

    Create config.py
    Added config file for Wifi and API-Key.

---
+++ inky_frame/config.py
@@ -0,0 +1,11 @@
+# Place your wifi password here.
+WIFI_SSID = "*****"
+WIFI_PASSWORD = "*****"
+# Replace with your actual OpenWeatherMap API key and city ID.
+API_KEY = "*****"
+# City ID for Dortmund.
+CITY_ID = "2935517"
+# Latitude for the selected city.
+LAT=51.49
+# Longitude for the selected city.
+LON=7.45
